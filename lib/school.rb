656416653b7f7e25717a9d26c06586a9aabe0eb2
github username <github email address>
2020-11-21 04:04:25 -0600

    commit tree b0ca1c33c28be6913036603d57d96f045e394871

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School 
+
+    attr_accessor :roster, :name 
+
+    def initialize(name)
+        @name = name 
+        @roster = {}
+    end 
+
+    def add_student(student, grade)
+        roster[grade] ||= []
+        roster[grade] << student
+    end 
+
+    def grade(number)
+        @roster[number]
+    end 
+
+
+    def sort 
+        @roster.each do |grade, name|
+            @roster[grade] = name.sort 
+        end 
+    end 
+end 
